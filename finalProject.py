5f46d289851667e80d0af85193d19f2a74a0f597
Mnngh05 <166870492+Mnngh05@users.noreply.github.com>
2025-05-19 00:22:31 +0330

    Add files via upload

---
+++ finalProject.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, key):
+        self.value = key  
+        self.left = None  
+        self.right = None  
+
+class BST:
+    Name = str()
+    Id = int()
+    def __init__(self):
+        self.root = None
+    def insertRequest(id, name):
+        if self.root is None:
+            self.root = self.n
